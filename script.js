2c4675f580250a04d49c510fc1be6992711480f5
gabrielramalhao <gabriel123.ramalhao@gmail.com>
2025-08-03 21:48:46 -0300

    feat: project rebuild

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const API = "https://economia.awesomeapi.com.br/json/last/";
-const https = require('https');
+const currency = process.argv[2];
+const url = `https://economia.awesomeapi.com.br/json/last/${currency}`;
 
-function fetch(url){
-  return new Promise((resolve, reject) => {
-    https.get(url, (resposta) => {
-      let data = '';
-      resposta.on('data', (chunk) => {
-        data += chunk;
-      });
-      resposta.on('end', () => {
-        resolve(JSON.parse(data));
-      });
-      resposta.on('error', (err) => {
-        reject(err);
-      });
-    });
-  });
+async function getData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const result = await response.json();
+    const item = result[`${currency.replace('-', '')}`];
+    console.log(`Moedas: ${item.name}`);
+    console.log(`Valor: ${item.bid}, Valor: 1`)
+
+  } catch (error) {
+    console.error(error.message);
+  }
 }
+
+getData(url);
